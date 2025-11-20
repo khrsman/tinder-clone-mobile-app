c4d503dc8f70cb81b7be56cfa2cc0175f769e7d3
khrsman <khrsman@gmail.com>
2025-11-20 14:52:17 +0700

    adjust layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -17,16 +17,16 @@ export default function TabLayout() {
         tabBarButton: HapticTab,
       }}>
       <Tabs.Screen
-        name="index"
+        name="main"
         options={{
           title: 'Home',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="likedOpponentList"
         options={{
-          title: 'Explore',
+          title: 'Liked Opponent List',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
