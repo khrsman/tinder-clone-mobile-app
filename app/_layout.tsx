58ce311540181ae3e526f967de58ef412d02dd58
khrsman <khrsman@gmail.com>
2025-11-20 19:01:46 +0700

    add gesture handler in app layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,9 @@
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { Appearance } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import 'react-native-reanimated';
+import { LikesProvider } from '../context/likes';
 
 export const unstable_settings = {
   anchor: '(tabs)',
@@ -10,11 +13,13 @@ export default function RootLayout() {
   const colorScheme = Appearance.getColorScheme();
 
   return (
-    <>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      </Stack>
-      <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'} />
-    </>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <LikesProvider>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        </Stack>
+        <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'} />
+      </LikesProvider>
+    </GestureHandlerRootView>
   );
 }
