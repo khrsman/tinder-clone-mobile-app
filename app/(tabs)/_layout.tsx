998f5f638e7a3040926f283e485147e6cfdb976e
khrsman <khrsman@gmail.com>
2025-11-21 03:04:30 +0700

    create custom tab navigaion

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,27 +1,47 @@
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import { Tabs } from 'expo-router';
-import React from 'react';
+import { Pressable, useWindowDimensions, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function TabLayout() {
+  function CustomTabBar({ state, navigation }: any) {
+    const { width } = useWindowDimensions();
+    const contentWidth = Math.min(width - 32, 520);
+    const isMainFocused = state.index === state.routes.findIndex((r: any) => r.name === 'main');
+    const isLikedFocused = state.index === state.routes.findIndex((r: any) => r.name === 'likedOpponentList');
+
+    return (
+      <SafeAreaView edges={['top']} style={{ backgroundColor: '#f5f5f7' }}>
+        <View style={{ width: contentWidth, alignSelf: 'center', flexDirection: 'row', justifyContent: 'space-around', 
+        alignItems: 'center',
+           marginBottom: 20 }}>
+          <Pressable
+            onPress={() => navigation.navigate('main')}
+            style={{ alignItems: 'center', justifyContent: 'center' }}
+          >
+            <MaterialIcons name="whatshot" size={48} color="#ff6b6b" />
+          </Pressable>
+          <Pressable
+            onPress={() => navigation.navigate('likedOpponentList')}
+            style={{ alignItems: 'center', justifyContent: 'center', position: 'relative' }}
+          >
+            <MaterialIcons name="auto-awesome" size={48} color="#8aa4bd" />
+          </Pressable>
+        </View>
+      </SafeAreaView>
+    );
+  }
+
   return (
     <Tabs
       screenOptions={{
         headerShown: false,
-      }}>
-      <Tabs.Screen
-        name="main"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <MaterialIcons size={28} name="home" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="likedOpponentList"
-        options={{
-          title: 'Liked Opponent List',
-          tabBarIcon: ({ color }) => <MaterialIcons size={28} name="send" color={color} />,
-        }}
-      />
+        tabBarShowLabel: false,
+      }}
+      tabBar={(props) => <CustomTabBar {...props} />}
+    >
+    
+     
     </Tabs>
   );
 }
