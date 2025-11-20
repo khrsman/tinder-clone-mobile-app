367ebf23d10485031d25dc4c76eb72da56386bb7
khrsman <khrsman@gmail.com>
2025-11-20 18:53:42 +0700

    add molecules photo indicator

--- app/(tabs)/main.tsx
+++ app/(tabs)/main.tsx
@@ -1,10 +1,8 @@
 import { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { ActionBar } from '../../components/organisms/ActionBar';
-
 import { useLikes } from '../../context/likes';
 import { opponents } from '../../data/opponents';
-
 
 export default function Main() {
   const [index, setIndex] = useState(0);
@@ -14,13 +12,12 @@ export default function Main() {
 
   const handleLeft = () => setIndex((i) => Math.min(opponents.length - 1, i + 1));
   const handleRight = () => {
-    like(current.id);
+    if (current) like(current.id);
     setIndex((i) => Math.min(opponents.length - 1, i + 1));
   };
 
   return (
     <View style={styles.container}>
-    
       <ActionBar onUndo={undo} onCancel={handleLeft} onLike={handleRight} />
     </View>
   );
